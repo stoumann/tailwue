8ae65d68991af7bd5064f54b02d60565770c751c
Mark Stoumann <mark@stoumann.dk>
2020-10-15 12:52:39 +0200

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { TailwueSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TailwueSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <tailwue-sample />
+  </div>
+</template>
